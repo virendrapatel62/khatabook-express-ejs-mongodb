e6e2b607c0ca3cc675ae800a4db4b2777e6530bb
Virendra patel <patelvirendra62@gmail.com>
2021-11-06 09:51:09 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+app.get("/", (request, response) => {
+  response.json({ message: "App is working." });
+});
