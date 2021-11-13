02cbc1c7c38538741a9f375c5a9fbf04516842fe
Virendra patel <patelvirendra62@gmail.com>
2021-11-13 08:14:05 +0530

    Password validation

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -7,6 +7,24 @@ const registerPageHandler = (request, response) => {
 
 const registerFormHandler = (request, response) => {
   console.log(request.body);
+  let errorMessage = null;
+  const { name, email, password, password2, phone } = request.body;
+  console.log({ name, email, password, password2, phone });
+
+  if (
+    !password ||
+    !password.trim() ||
+    !password2 ||
+    !password2.trim() ||
+    password.trim() !== password2.trim()
+  ) {
+    errorMessage = "password and password2 must be same.";
+  }
+
+  if (errorMessage) {
+    response.render("pages/register", { errorMessage });
+  }
+
   response.redirect("/login");
 };
 
