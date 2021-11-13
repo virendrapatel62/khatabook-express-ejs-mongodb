6f5c8e9fff1d271a172c0f7c2f91c84d0d7a79ae
Virendra patel <patelvirendra62@gmail.com>
2021-11-13 10:26:57 +0530

    Registration

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,3 +1,6 @@
+const { User } = require("../models");
+const passwordHash = require("password-hash");
+
 const loginPagehandler = (request, response) => {
   response.render("pages/login");
 };
@@ -5,13 +8,14 @@ const registerPageHandler = (request, response) => {
   response.render("pages/register");
 };
 
-const registerFormHandler = (request, response) => {
-  console.log(request.body);
+function validateUserRegistrationData(formValues) {
+  const { name, email, password, password2, phone } = formValues;
   let errorMessage = null;
-  const { name, email, password, password2, phone } = request.body;
-  console.log({ name, email, password, password2, phone });
-
-  if (
+  if (!name || !name.trim()) {
+    errorMessage = "Name is required";
+  } else if (!email || !email.trim()) {
+    errorMessage = "Email is required";
+  } else if (
     !password ||
     !password.trim() ||
     !password2 ||
@@ -19,13 +23,44 @@ const registerFormHandler = (request, response) => {
     password.trim() !== password2.trim()
   ) {
     errorMessage = "password and password2 must be same.";
+  } else if (!phone || !phone.trim()) {
+    errorMessage = "Phone is required";
   }
 
-  if (errorMessage) {
-    response.render("pages/register", { errorMessage });
+  return {
+    error: errorMessage,
+    user: errorMessage
+      ? null
+      : {
+          email: email.trim(),
+          name: name.trim(),
+          phone: phone.trim(),
+          password: password.trim(),
+        },
+  };
+}
+
+const registerFormHandler = (request, response) => {
+  console.log(request.body);
+  const formValues = request.body;
+
+  const { error, user } = validateUserRegistrationData(formValues);
+  if (error) {
+    const context = { error, formValues };
+    return response.render("pages/register", context);
   }
 
-  response.redirect("/login");
+  User.create({
+    ...user,
+    password: passwordHash.generate(user.password),
+  })
+    .then((user) => {
+      response.redirect("/login");
+    })
+    .catch((error) => {
+      const context = { error: error.message, formValues };
+      return response.render("pages/register", context);
+    });
 };
 
 module.exports = {
