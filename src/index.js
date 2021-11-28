6041e1439d3628773fdd7c95a2e5c9ff3a1c7174
Virendra patel <patelvirendra62@gmail.com>
2021-11-28 12:47:28 +0530

    Session Example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const router = require("./routers");
 const { createConnection } = require("./db");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 // Mongo Db Connection
 createConnection()
@@ -15,17 +16,31 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "123456@123#@#$",
+  })
+);
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
 app.use(router);
 
-app.get("/set", (req, res) => {
-  res.cookie("name", "virendra");
-  res.cookie("time", new Date().toTimeString());
+app.get("/page/:name/:age", (req, res) => {
+  const { name, age } = req.params;
+  req.session.user = {
+    name,
+    age,
+  };
   res.render("sample");
 });
-app.get("/get", (req, res) => {
+
+app.get("/page1", (req, res) => {
+  console.log(req.session);
   res.render("sample", { request: req });
+});
+
+app.get("/page2", (req, res) => {
+  res.render("sample");
 });
