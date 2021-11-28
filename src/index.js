1ea6ef573adcbd8f251c49257bc79c588eaf7e65
Virendra patel <patelvirendra62@gmail.com>
2021-11-28 12:29:39 +0530

    cookie demo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const router = require("./routers");
 const { createConnection } = require("./db");
-const { User, Khatabook, Customer, Entry } = require("./models");
+const cookieParser = require("cookie-parser");
 
 // Mongo Db Connection
 createConnection()
@@ -14,8 +14,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 
 app.use(router);
+
+app.get("/set", (req, res) => {
+  res.cookie("name", "virendra");
+  res.cookie("time", new Date().toTimeString());
+  res.render("sample");
+});
+app.get("/get", (req, res) => {
+  res.render("sample", { request: req });
+});
