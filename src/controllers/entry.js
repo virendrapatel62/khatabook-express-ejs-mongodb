3b884fe7d46cce7f3ca45bae403ac939946ce62f
Virendra patel <patelvirendra62@gmail.com>
2021-12-23 08:02:49 +0530

    khatabook selector and customer selector

--- src/controllers/entry.js
+++ src/controllers/entry.js
@@ -1,4 +1,4 @@
-const { Khatabook } = require("../models");
+const { Khatabook, Customer } = require("../models");
 
 const khatabookSelectorHandler = async (request, response) => {
   const user = request.session.user;
@@ -6,8 +6,23 @@ const khatabookSelectorHandler = async (request, response) => {
   const context = {
     request,
     khatabooks,
+    render: "khatabook-selector",
   };
   return response.render("pages/entries", context);
 };
 
-module.exports = { khatabookSelectorHandler };
+const customerSelectorPage = async (request, response) => {
+  const khatabookId = request.params.khatabook;
+  console.log("SELECTED KHATABOOK IS", khatabookId);
+  const customers = await Customer.find({ khatabook: khatabookId });
+  console.log(customers);
+  const context = {
+    request,
+    customers,
+    selectedKhatabook: khatabookId,
+    render: "customer-selector",
+  };
+  return response.render("pages/entries", context);
+};
+
+module.exports = { khatabookSelectorHandler, customerSelectorPage };
--- src/routers/entry.js
+++ src/routers/entry.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { khatabookSelectorHandler } = require("../controllers/entry");
+const {
+  khatabookSelectorHandler,
+  customerSelectorPage,
+} = require("../controllers/entry");
 const { userAuthMiddleware } = require("../middlewares");
 const entryRouter = express.Router();
 
 // localhost:3000/entries
 
 entryRouter.get("/", userAuthMiddleware, khatabookSelectorHandler);
+entryRouter.get(
+  "/khatabook/:khatabook",
+  userAuthMiddleware,
+  customerSelectorPage
+);
 
 module.exports = {
   entryRouter,
