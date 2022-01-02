70d44b83356c1024d7c6549f1b5e7514121a2771
Virendra patel <patelvirendra62@gmail.com>
2022-01-02 16:48:15 +0530

    dashboard

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,4 +1,4 @@
-const { Khatabook, Customer } = require("../models");
+const { Khatabook, Customer, Entry } = require("../models");
 
 const dashboardPageHandler = async (request, response) => {
   const loggedInUser = request.session?.user?._id;
@@ -7,6 +7,43 @@ const dashboardPageHandler = async (request, response) => {
   for (let khatabook of khatabooks) {
     const count = await Customer.countDocuments({ khatabook: khatabook.id });
     khatabook.customerCount = count;
+
+    // cal calculate total get and give
+    const customers = await Customer.find({ khatabook: khatabook })
+      .select("id")
+      .then((customers) => customers.map((customer) => customer._id));
+
+    console.log(customers);
+
+    const gaveGotSummary = await Entry.aggregate()
+      .match({ customer: { $in: customers } })
+      .group({
+        _id: "$customer",
+        totalGave: { $sum: "$youGave" },
+        totalGot: { $sum: "$youGot" },
+      })
+      .then((result) =>
+        result.reduce(
+          (acc, item) => {
+            acc.totalGave += item.totalGave;
+            acc.totalGot += item.totalGot;
+            return acc;
+          },
+          { totalGave: 0, totalGot: 0 }
+        )
+      );
+
+    const { totalGave, totalGot } = gaveGotSummary;
+    if (totalGave > totalGot) {
+      khatabook.youWillGive = 0;
+      khatabook.youWillGet = totalGave - totalGot;
+    } else if (totalGave < totalGot) {
+      khatabook.youWillGet = 0;
+      khatabook.youWillGive = totalGot - totalGave;
+    } else {
+      khatabook.youWillGet = 0;
+      khatabook.youWillGive = 0;
+    }
   }
 
   const context = {
